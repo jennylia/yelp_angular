faa3a4a72eb640e238afed7c1db7ae8ebad71cdb
Jenny Lian <jenny@fingerfoodstudios.com>
2015-10-22 16:04:24 -0700

    added the exposable parameter in the front end, yelp factor

--- js/services/yelpFactory.js
+++ js/services/yelpFactory.js
@@ -1,5 +1,10 @@
 app.factory('yelpFactory',['$http', function($http){
-	var url = 'http://localhost:3000/yelp';
+	var data = {
+		location: "vancouver"
+	};
+	var city = "Burnaby";
+	var location = "?location=" + city;
+	var url = 'http://localhost:3000/yelp' + location;
 	return $http.get(url).success(function(data){
 		return data;
 	}).error(function(error){
